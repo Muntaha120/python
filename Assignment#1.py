94fed8236c3cd0c28b6649cb32c43250c1a4e095
Muntaha120 <164341249+Muntaha120@users.noreply.github.com>
2024-03-21 04:18:08 -0700

    Add files via upload

---
+++ Assignment#1.py
@@ -0,0 +1,33 @@
+#Question#1
+numb = 1
+if numb > 0:
+    print('number is positive')
+elif numb<0:
+    print('number is negative')
+else:
+    print('number is zero')
+
+#Question#2
+a = 9
+b = 2
+c = 6
+if a>b and a>c:
+    print('a is largest')
+elif b>a and b>c:
+    print('b is largest')
+else:
+    print("c is largest")
+
+#Question#3
+year = 2016
+if year%4 == 0:
+    print('year is leap year')
+else:
+    print('year is not a leap year')
+
+#Question#4
+alphabet = 'a'
+if alphabet == 'a' or alphabet == 'e' or alphabet == 'i' or alphabet == 'o' or alphabet == 'u':
+    print('alphabet is vowel')
+else:
+    print('alphabet is consonant')
